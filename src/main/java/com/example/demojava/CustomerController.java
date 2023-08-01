1cfda13901bb9ee3b7f228e35ceaa2ebd9e5d9af
tfrank <tfrank@vmware.com>
2023-08-01 09:02:50 -0400

    first commit

---
+++ src/main/java/com/example/demojava/CustomerController.java
@@ -0,0 +1,18 @@
+package com.example.demojava;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/customers")
+public class CustomerController {
+
+    @GetMapping
+    public List<Customer> customers() {
+        return List.of(new Customer("Frank Tyler"), new Customer("Denise Tyler"));
+    }
+
+}
